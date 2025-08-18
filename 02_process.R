362468e278ff7711addcc2e8e98ce45dfd742a5d
njlyon0 <njlyon@alumni.iastate.edu>
2025-08-18 15:09:43 -0400

    feat (process): script skeleton

---
+++ 02_process.R
@@ -0,0 +1,22 @@
+## --------------------------------------------------------- ##
+# Zhong Grasshopper - Process Data
+## --------------------------------------------------------- ##
+
+# Purpose
+## Generate desired summary metrics (and other pre-stats wrangling)
+
+## --------------------------------- ##
+# Housekeeping ----
+## --------------------------------- ##
+
+# Load libraries
+librarian::shelf(tidyverse, supportR)
+
+# Clear environment
+rm(list = ls()); gc()
+
+
+
+
+
+# End ----
