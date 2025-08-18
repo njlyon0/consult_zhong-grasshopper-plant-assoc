d42f97f4789dc593a69dc91056debf0612625aab
njlyon0 <njlyon@alumni.iastate.edu>
2025-08-18 11:34:46 -0400

    feat (setup): first pass at making needed folders and input downloads

--- 00_setup.R
+++ 00_setup.R
@@ -17,15 +17,26 @@ librarian::shelf(tidyverse, googledrive)
 rm(list = ls()); gc()
 
 ## --------------------------------- ##
-# -----
+# Make Folder(s) ----
 ## --------------------------------- ##
 
+# Create desired folder(s)
+dir.create(path = file.path("data", "raw"), showWarnings = F, recursive = T)
+dir.create(path = file.path("graphs"), showWarnings = F)
 
 ## --------------------------------- ##
-# -----
+# Download Input(s) ----
 ## --------------------------------- ##
 
+# Identify Drive folder
+zhong_drive <- googledrive::as_id("https://drive.google.com/drive/u/0/folders/1tlyKbUULHHEB5FzwmxqWziavfpwfU7tJ")
 
+# List files in that folder
+(zhong_files <- googledrive::drive_ls(path = zhong_drive))
 
+# Download 'em
+purrr::walk2(.x = zhong_files$id, .y = zhong_files$name,
+            .f = ~ googledrive::drive_download(file = .x, overwrite = T,
+                                               path = file.path("data", "raw", .y)))
 
 # End ----
