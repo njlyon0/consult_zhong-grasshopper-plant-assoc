202730ffcdeb96d8fd0afcd34f55e9f49b5e3ca5
njlyon0 <njlyon@alumni.iastate.edu>
2025-08-19 09:26:15 -0400

    feat (viz): script skeleton

---
+++ 04_figures.R
@@ -0,0 +1,25 @@
+## --------------------------------------------------------- ##
+# Zhong Grasshopper - Visualize Data
+## --------------------------------------------------------- ##
+
+# Purpose
+## Generate the desired publication-quality figures
+
+## --------------------------------- ##
+# Housekeeping ----
+## --------------------------------- ##
+
+# Load libraries
+librarian::shelf(tidyverse)
+
+# Clear environment
+rm(list = ls()); gc()
+
+## --------------------------------- ##
+# ----
+## --------------------------------- ##
+
+
+
+# End ----
+
